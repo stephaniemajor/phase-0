cd22b1101a25aafcb9ca51c35b3da9ddc6ba0712
Stephanie Major <stephaniemajor99@hotmail.com>
2016-02-11 05:00:04 +0000

    Add module file 7.6 pair exercise Olympics

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,84 @@
+ // JavaScript Olympics
+
+// I paired Thomas on this challenge.
+
+// This challenge took me 1.5 hours.
+
+
+// Warm Up
+// Create an array with athlete names and their events
+// var athletes = [
+//     {name: "Athlete#1",
+//     event: "swimming"
+//     },
+//     {name: "Atlete#2",
+//      event: "skating"
+//      }
+//  ]
+
+// Bulk Up
+//  add to the array a new property called win 
+// athletes.win = "#{athletes.name} won the #{athletes.event}!"
+  
+// console.log(athletes)
+
+
+// Jumble your words
+// create a function to reverse a string
+// function reverse(string) {
+//   var o = '';
+//   for (var i = string.length - 1; i >= 0; i--)
+//     o += string[i];
+//   return o;
+// }
+//  console.log(reverse("hello"))
+
+
+
+// 2,4,6,8
+//  Create a function for an array of numbers
+//  function even(numbers) {
+//    var newNumbers = [];
+//    for (var i = 0; i < numbers.length; i++ )
+//      if (numbers % 2 == 0) {
+//        newNumbers.push(numbers[i]);
+//      }
+//         return newNumbers;
+//  }
+// console.log(even(1,2,3,4,5,6));
+
+
+
+// "We built this city"
+// new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+// function michaelPhelps(){
+//   name = "Michael Phelps",
+//   age = 29,
+//   profession = "swimming",
+//   string = "It's medicinal I swear!"
+// }
+
+// console.log(michaelPhelps)
+
+// console.log(michaelPhelps.constructor === Athlete)
+// console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+// 
+// function Athlete(name, age, sport, quote){
+//   this.name = name;
+//   this.age = age;
+//   this.sport = sport;
+//   this.quote = quote;
+// };
+
+// console.log(Athlete)("michael", 30, "swim", "splash")
+
+
+
+// Reflection
+// •	What JavaScript knowledge did you solidify in this challenge?
+// Not to forget the parenthesis for the if, and the semi colon for the line items
+// •	What are constructor functions?
+// It has the “this” part with each of the properties. I didn’t really get it.
+// •	How are constructors different from Ruby classes (in your research)?
+// The “this” is additional. 
