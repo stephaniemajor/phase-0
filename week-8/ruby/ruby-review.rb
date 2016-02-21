d66860520a1a6ad4557ca261b9703ed8ad283c0f
Stephanie Major <stephaniemajor99@hotmail.com>
2016-02-21 18:25:48 +0000

    Update ruby challenge week-8 with reflection

--- week-8/ruby/ruby-review.rb
+++ week-8/ruby/ruby-review.rb
@@ -36,4 +36,10 @@ puts super_fizzbuzz([1,3,4,5,6,80,90,75,84,86,27,30])
 
 # Reflection
 # I am happy I did my cheat sheet code first!
-# It was easier than I thought?
+# # It was easier than I thought?
+# What concepts did you review or learn in this challenge?
+# I reviewed the iteration
+# What is still confusing to you about Ruby?
+# I am Ok with this exercise
+# What are you going to study to get more prepared for Phase 1?
+# I will do at least one other Ruby exercise
