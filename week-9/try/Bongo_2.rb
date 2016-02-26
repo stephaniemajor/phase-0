43ca5e187dd9e72c712091edd32c894e7f936057
Stephanie Major <stephaniemajor99@hotmail.com>
2016-02-26 17:03:12 +0000

    try folder for current work

---
+++ week-9/try/Bongo_2.rb
@@ -0,0 +1,59 @@
+# Create a Bingo Scorer (SOLO CHALLENGE)
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+# input: two dimensional array
+# output: boolean, "BINGO!"
+# Create a method to iterate through an array of arrays if one array has only Xs it will print "BINGO!"
+# Create a method to iterate through an array of arrays if a position for each inner array has only Xs,it will print "BINGO!" 
+# Create a method to iterate through an array of arrays if a position with is the same index for the array for all has only Xs,it will print "BINGO!" 
+# Create a method to iterate through an array of arrays if the max position of the min index has an X and then iterate for position minus one and index + 1, it will rpint "BINGO"
+
+# sample boards
+
+# horizontal = [[47, 44, 71, 8, 88],
+#               ['x', 'x', 'x', 'x', 'x'],
+#               [83, 85, 97, 89, 57],
+#               [25, 31, 96, 68, 51],
+#               [75, 70, 54, 80, 83]]
+
+# vertical = [[47, 44, 71, 'x', 88],
+#             [22, 69, 75, 'x', 73],
+#             [83, 85, 97, 'x', 57],
+#             [25, 31, 96, 'x', 51],
+#             [75, 70, 54, 'x', 83]]
+
+
+# right_to_left = [['x', 44, 71, 8, 88],
+#                  [22, 'x', 75, 65, 73],
+#                  [83, 85, 'x', 89, 57],
+#                  [25, 31, 96, 'x', 51],
+#                  [75, 70, 54, 80, 'x']]
+
+
+# left_to_right = [[47, 44, 71, 8, 'x'],
+#                   [22, 69, 75, 'x', 73],
+#                   [83, 85, 'x', 89, 57],
+#                   [25, 'x', 96, 68, 51],
+#                   ['x', 70, 54, 80, 83]]
+
+
+
+
+# Initial Solution
+class BingoScorer
+
+  #your code here
+# Create a method to recognize an array with Xs that will print "BINGO!"
+
+
+
+
+
+end
+
+
+
+
+# Refactored Solution
